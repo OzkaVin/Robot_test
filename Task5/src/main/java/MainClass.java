87c49478e1e233dd6c9ff08ec38c719b5da07071
Olha S <53303282+OzkaVin@users.noreply.github.com>
2022-02-16 18:47:58 +0200

    task 5

---
+++ Task5/src/main/java/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+    public static void main(String[] args) {
+        OtherClass classInstance = new OtherClass();
+
+        classInstance.printNumber();
+        System.out.println("~~~ <возвращенное значение> \"~~~");
+    }
+}
---
+++ Task5/src/main/java/OtherClass.java
@@ -0,0 +1,7 @@
+public class OtherClass {
+    int number = 25;
+
+    public void printNumber() {
+        System.out.println(String.format("---<значение поля>---%s", number));
+    }
+}
